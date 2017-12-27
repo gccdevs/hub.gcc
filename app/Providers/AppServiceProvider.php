eb8f47eabb62bc99f9c6628569fe6e152ad0f22a
Tony <gaohaoqian@gmail.com>
2017-12-28 02:21:24 +1100

    Created A HTTPS Middleware to force redirection
    This is the real way to solve HTTPS redirection in software layer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
         Stripe::setApiKey(config('services.stripe.secret'));
     }
 
